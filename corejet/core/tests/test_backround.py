8020cd62053856cc99a6108b9c91112f1a3ca0db
Asko Soukka <asko.soukka@iki.fi>
2011-09-21 08:41:49 +0300

    Added setBackground-test for story without background.

--- corejet/core/tests/test_backround.py
+++ corejet/core/tests/test_backround.py
@@ -325,8 +325,78 @@ class Story2(unittest.TestCase):
        title=u"As a developer, I can parse my scenarios with background from text")
 class Story3(unittest.TestCase):
 
+    @scenario(u"Without background")
+    class ScenarioA(Scenario):
+
+        @given(u"A requirements catalogue containing user requirements")
+        def given(self):
+            from corejet.core.model import RequirementsCatalogue, Epic, Story
+
+            self.catalogue = RequirementsCatalogue(project="Test project",
+                    extractTime=datetime.datetime(2011, 1, 2, 12, 1, 0))
+
+            epic1 = Epic("E1", "First epic")
+            self.catalogue.epics.append(epic1)
+
+            epic2 = Epic("E2", "Second epic")
+            self.catalogue.epics.append(epic2)
+
+            story1 = Story("S1", "First story", points=3, status="open",
+                priority="high", epic=epic1)
+            epic1.stories.append(story1)
+
+            story2 = Story("S2", "Second story", points=3, status="closed",
+                resolution="fixed", priority="high", epic=epic1)
+            epic1.stories.append(story2)
+
+            text = """\
+Scenario: First scenario
+Given something
+When something happens
+Then do something
+  And something else
+
+Scenario: Second scenario
+Given something
+When something happens
+Then do something
+  And something else
+"""
+            setBackground(story1, text)
+            appendScenarios(story1, text)
+
+        @when(u"The serialize() method is called")
+        def when(self):
+            self.tree = self.catalogue.serialize()
+
+        @then(u"An XML tree is output containing all epics, stories "
+              u"with background and scenarios")
+        def then(self):
+            self.assertEqual(
+                lxml.etree.tostring(self.tree, pretty_print=True).strip(), """\
+<requirementscatalogue project="Test project" extractTime="2011-01-02T12:01:00">
+  <epic id="E1" title="First epic">
+    <story id="S1" title="First story" points="3" requirementStatus="open" priority="high">
+      <scenario name="First scenario">
+        <given>something</given>
+        <when>something happens</when>
+        <then>do something</then>
+        <then>something else</then>
+      </scenario>
+      <scenario name="Second scenario">
+        <given>something</given>
+        <when>something happens</when>
+        <then>do something</then>
+        <then>something else</then>
+      </scenario>
+    </story>
+    <story id="S2" title="Second story" points="3" requirementStatus="closed" requirementResolution="fixed" priority="high"/>
+  </epic>
+  <epic id="E2" title="Second epic"/>
+</requirementscatalogue>""".strip())
+
     @scenario(u"Requirements extract")
-    class Scenario(Scenario):
+    class ScenarioB(Scenario):
 
         @given(u"A requirements catalogue containing user requirements")
         def given(self):
